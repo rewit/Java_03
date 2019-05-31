e055e6e6a3901ae728078b326569c489d1e4c2e1
rewit <rewit@naver.com>
2019-05-31 16:34:57 +0900

    Java_03

---
+++ src/Gugu_01.java
@@ -0,0 +1,54 @@
+
+public class Gugu_01 {
+
+	public static void main(String[] args) {
+		
+		
+		//3단
+		int intNum1 = 1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("3x"+intNum1 + "=" +3*intNum1);
+		
+		//4단
+		
+		intNum1 = 1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		intNum1 = intNum1+1;
+		System.out.println("4x"+intNum1 + "=" +4*intNum1);
+		
+
+		
+		
+		
+	}
+
+}
